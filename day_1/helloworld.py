d46ef3347b3a48d32145f37dbf923b70832a4545
Rene Baine <renebaine04@gmail.com>
2026-07-11 15:20:48 -0400

    Euclidean distance between two points

--- day_1/helloworld.py
+++ day_1/helloworld.py
@@ -1,4 +1,5 @@
 # Day 1 - 30 day python challenge
+import math
 
 print(2 + 3)             # addition(+)
 print(3 - 1)             # subtraction(-)
@@ -17,3 +18,21 @@ print(type([1, 2, 3]))   # List
 print(type({'name':'Asabeneh'})) # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set
 print(type((9.8, 3.14, 2.7)))    # Tuple
+
+
+# Finding the Euclidean distance between one and twe dimensiona points
+# Import the math library
+
+import math
+
+p = [3]
+q = [1]
+
+#Calculate the Euclidean distance
+
+print(math.dist(p,q))
+
+p = [3, 3]
+q = [6 , 12]
+
+print(math.dist(p,q))
