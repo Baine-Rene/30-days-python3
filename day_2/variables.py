a905c035e493070c47a21f6b7db2e15d5d3113b5
Rene Baine <renebaine04@gmail.com>
2026-07-13 15:34:19 -0400

    CHecking variable types with type() function

---
+++ day_2/variables.py
@@ -0,0 +1,29 @@
+# Day 1 - 30 day python challenge
+
+first_name = 'Rene'
+last_name = 'Baine'
+full_name = 'Rene Baine'
+country = 'Uganda'
+city = 'Kampala'
+age = '21'
+year = '2026'
+is_married = 'No'
+is_true = False
+is_light_on = False
+x, y, z = "apple", "banana", "orange"
+
+# Checking variable types
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(x))
+print(type(y))
+print(type(z))
