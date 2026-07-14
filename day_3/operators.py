4e1c905ce01a8364299bf3eb909f4ec7f6f74729
Rene Baine <renebaine04@gmail.com>
2026-07-14 15:27:33 -0400

    Calculated area of triangle with user input

---
+++ day_3/operators.py
@@ -0,0 +1,11 @@
+# Day 3 - 30 day python challenge
+
+age = 21
+height = 175.00
+complex_num = (1 + 1j)
+
+base = input("Enter the base of the triangle: ")
+height = input("Enter the height of the triangle: ")
+
+area = 0.5 * int(base) * int(height)
+print('The area of the triangle is: ',area)
