aa6cc489c1c8cdbc266ef031a4860f89a8da2a10
Rene Baine <renebaine04@gmail.com>
2026-07-14 15:34:26 -0400

    Calculated the permimeter of the traingle

--- day_3/operators.py
+++ day_3/operators.py
@@ -9,3 +9,14 @@ height = input("Enter the height of the triangle: ")
 
 area = 0.5 * int(base) * int(height)
 print('The area of the triangle is: ',area)
+
+# Calculating the perimeter of the triangle
+
+side_a = input("Enter side a:")
+side_b = input("Enter side b:")
+side_c = input("Enter side c:")
+
+perim = int(side_a) + int(side_b) + int(side_c)
+
+print('The perimeter for the triangle is:', perim)
+
