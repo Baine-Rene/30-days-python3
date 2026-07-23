725374549c0bc9b19b9197f7abac9d8bba951478
Rene Baine <renebaine04@gmail.com>
2026-07-23 13:49:39 -0400

    String manipulation upper and lower

--- day_4/strings.py
+++ day_4/strings.py
@@ -1,6 +1,19 @@
 # Day 4 - 30 day python challenge
 
-space = ' '
 
+space = ' '
 title = 'Thirty' + space + 'Days' + space + 'of' + space + 'Python'
 print(title)
+
+coding = 'Coding' + ' for' + ' all'
+print(coding)
+
+company = coding
+print(len(company))
+
+capital = company.upper()
+print(capital)
+
+lower = company.lower()
+print(lower)
+
