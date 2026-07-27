17b7ecead68956e0103753a78f3701b0c6ecda71
Rene Baine <renebaine04@gmail.com>
2026-07-27 14:48:21 -0400

    List manipulation

--- day_5/lists.py
+++ day_5/lists.py
@@ -1,3 +1,29 @@
-roomates = ['rene', 'ruth', 'alana']
-print('There are', len(roomates), 'roomates')
+# Day 5 - 30 day python challenge
 
+empty_list = []
+
+stationary = ['pens', 'markers', 'pencils', 'ruler', 'brush', 'ink']
+print(len(stationary))
+
+print(stationary[0])
+print(stationary[3])
+print(stationary[5])
+
+mixed_data_types = ['Rene Baine', 22, {'height': 175, 'Marita_Status': 'Not Married', 'Address': '571 reid street'}]
+
+companies = ['Apple', 'IBM', 'Oracle', 'Amazon', 'Google']
+
+print(mixed_data_types)
+print(companies)
+
+print('The number of companies on the list is', len(companies))
+
+companies[0] = 'Nvidia'
+
+companies.append('Microsoft')
+
+print(companies[0])
+print(companies[2])
+print(companies[4])
+
+print(companies)
